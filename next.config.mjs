10baea06531d50c1b5541fc8efd1dfdc648b9807
Dueen Eduarda <dueen.eduarda@gmail.com>
2022-07-15 21:00:57 +0200

    Massive package cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,5 @@
-import withMDX from "@next/mdx";
-import remarkCommentConfig from "remark-comment-config";
-import remarkRemoveComments from "remark-remove-comments";
-import remarkGithub from "remark-github";
-import remarkGfm from "remark-gfm";
-import remarklinkifyRegex from "remark-linkify-regex";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ["tsx", "mdx"],
 };
-
-const mdxOptions = {
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [
-      // remarkGithub({
-      //   repository: "rustlang/this-week-in-rust",
-      // }),
-      // remarkGfm,
-      // remarkRemoveComments,
-    ],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    providerImportSource: "@mdx-js/react",
-  },
-};
-
-export default withMDX(mdxOptions)(nextConfig);
+export default nextConfig;
